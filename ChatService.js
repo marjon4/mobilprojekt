c4e5f950dbd4eb221a488072fa2deb4444b1e998
Lina Modin Larsson <Lina@n150-p5.kthopen.kth.se>
2016-05-16 14:08:12 +0200

    chatservice

--- ChatService.js
+++ ChatService.js
@@ -1,4 +1,61 @@
 uniChat.factory('ChatService', function ($resource) {
 
+this.courses = [
+		{
+            name:"Algebra och geometri",
+			id : "SF1624",
+			comment : [],
+	},
+	{
+            name:"Envariabelanalys",
+			id : "SF1625",
+			comment : [],
+	},
+		{
+            name:"Flervariabelanalys",
+			id : "SF1626",
+			comment : [],
+	},
+		{
+            name:"Programmeringsteknik",
+			id : "DD1310",
+			comment : [],
+	},
+		{
+            name:"Sannolikhetsteori och statistik",
+			id : "SF1901",
+			comment : [],
+	},
+		{
+            name:"Tillämpad datalogi",
+			id : "DD1320",
+			comment : [],
+	},
+		{
+            name:"Databasteknik",
+			id : "DD1334",
+			comment : [],
+	},
+		{
+            name:"XML för publicering",
+			id : "DM2517",
+			comment : [],
+	},
+		{
+            name:"Mobilutveckling med webbteknologier",
+			id : "DM2518",
+			comment : [],
+	},
+		{
+            name:"Interaktionsprogrammering och dynamiska webben",
+			id : "DH2642",
+			comment : [],
+	}
+                    ];
 
+    this.getCourses = function(){
+    	return this.courses;
+    };
+
+return this;
 });
--- homeCtrl.js
+++ homeCtrl.js
@@ -1,5 +1,7 @@
 uniChat.controller('homeCtrl', function($scope, ChatService) {
 
-
+	$scope.courses = function(){
+		return ChatService.getCourses();		
+	}
 
 });
