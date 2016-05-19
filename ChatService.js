fae3a96e06e7c70d16f9e4365153322283bf596b
Lina Modin Larsson <Lina@n129-p82.kthopen.kth.se>
2016-05-19 14:01:59 +0200

    firebase

--- ChatService.js
+++ ChatService.js
@@ -1,93 +1,38 @@
 uniChat.factory('ChatService', function ($resource) {
 
 this.uni = "Start";
+var courses =[];
 
-this.courses = [
-		{
-            name:"Algebra och geometri",
-			id : "SF1624",
-			comment : [],
-			status : 0,
-	},
-	{
-            name:"Envariabelanalys",
-			id : "SF1625",
-			comment : [],
-			status : 0,
-	},
-		{
-            name:"Flervariabelanalys",
-			id : "SF1626",
-			comment : [],
-			status : 0,
-	},
-		{
-            name:"Programmeringsteknik",
-			id : "DD1310",
-			comment : [],
-			status : 0,
-	},
-		{
-            name:"Sannolikhetsteori och statistik",
-			id : "SF1901",
-			comment : [],
-			status : 0,
-	},
-		{
-            name:"Tillämpad datalogi",
-			id : "DD1320",
-			comment : [],
-			status : 0,
-	},
-		{
-            name:"Databasteknik",
-			id : "DD1334",
-			comment : [],
-			status : 0,
-	},
-		{
-            name:"XML för publicering",
-			id : "DM2517",
-			comment : [],
-			status : 0,
-	},
-		{
-            name:"Mobilutveckling med webbteknologier",
-			id : "DM2518",
-			comment : [],
-			status : 0,
-	},
-		{
-            name:"Interaktionsprogrammering och dynamiska webben",
-			id : "DH2642",
-			comment : [],
-			status : 0,
-	},
-		{
-			name : "Other courses",
-			id : "XXXX",
-			comment : [],
-			status : 0,
-	}
-                    ];
+var myFirebaseRef = new Firebase("https://mobunichat.firebaseio.com/");
+
+
+	myFirebaseRef.on("value", function(snapshot) {
+  	data = snapshot.val().courses;
+  	courses = [];
+
+  	for(i in data){
+  		courses.push(data[i])
+  		}
+	});
+
 
     this.getCourses = function(){
-    	return this.courses;
+    	return courses;
     };
 
     this.setSelectedCourse = function(id){
-    	for(i in this.courses){
-		this.courses[i].status = 0
-    		if(this.courses[i].id === id){
-    			this.courses[i].status = 1;
+    	for(i in courses){
+		courses[i].status = 0
+    		if(courses[i].id === id){
+    			courses[i].status = 1;
     		}
     	}
     };
 
     this.getSelectedCourse = function(){
-    	for(i in this.courses){
-    		if(this.courses[i].status === 1){
-    			return this.courses[i].name;
+    	for(i in courses){
+    		if(courses[i].status === 1){
+    			return courses[i].name;
     		}
     	}
     };
